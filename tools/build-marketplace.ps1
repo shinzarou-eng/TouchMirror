1651770c08cdcac04469e053c331dd685b4bfa70
shinzarou-eng <228665101+shinzarou-eng@users.noreply.github.com>
2026-09-26 11:29:56 +0200

    fix: index.json emis manuellement pour etre identique en ps5 et ps7

--- tools/build-marketplace.ps1
+++ tools/build-marketplace.ps1
@@ -24,9 +24,41 @@ Get-ChildItem "$root\marketplace" -Directory | Sort-Object Name | ForEach-Object
     }
 }
 
-$json = @{ plugins = $plugins } | ConvertTo-Json -Depth 5
-$json = [regex]::Replace($json, '[^\x00-\x7F]', { '\u{0:x4}' -f [int][char]$args[0].Value })
-$json = [regex]::Replace($json, "['<>&+``]", { '\u{0:x4}' -f [int][char]$args[0].Value })
-$json = $json -replace "`r`n", "`n"
-[System.IO.File]::WriteAllText("$root\marketplace\index.json", $json, [System.Text.UTF8Encoding]::new($false))
+$esc = {
+    param($v)
+    $s = "$v" -replace '\\', '\\' -replace '"', '\"'
+    $s = [regex]::Replace($s, '[^\x00-\x7F]', { '\u{0:x4}' -f [int][char]$args[0].Value })
+    [regex]::Replace($s, "['<>&+``]", { '\u{0:x4}' -f [int][char]$args[0].Value })
+}
+
+$lines = [System.Collections.Generic.List[string]]::new()
+$lines.Add('{')
+$lines.Add('    "plugins": [')
+$first = $true
+foreach ($p in $plugins) {
+    if (!$first) { $lines[$lines.Count - 1] += ',' }
+    $first = $false
+    $lines.Add('        {')
+    $lines.Add('            "id": "' + (& $esc $p.id) + '",')
+    $lines.Add('            "name": "' + (& $esc $p.name) + '",')
+    $lines.Add('            "version": "' + (& $esc $p.version) + '",')
+    $lines.Add('            "author": "' + (& $esc $p.author) + '",')
+    $lines.Add('            "icon": "' + (& $esc $p.icon) + '",')
+    $lines.Add('            "description": "' + (& $esc $p.description) + '",')
+    $lines.Add('            "official": ' + $p.official.ToString().ToLowerInvariant() + ',')
+    $lines.Add('            "featured": ' + $p.featured.ToString().ToLowerInvariant() + ',')
+    $lines.Add('            "tags": [')
+    $t = 0
+    foreach ($tag in $p.tags) {
+        $t++
+        $comma = if ($t -lt $p.tags.Count) { ',' } else { '' }
+        $lines.Add('                "' + (& $esc $tag) + '"' + $comma)
+    }
+    $lines.Add('            ],')
+    $lines.Add('            "hash": "' + $p.hash + '"')
+    $lines.Add('        }')
+}
+$lines.Add('    ]')
+$lines.Add('}')
+[System.IO.File]::WriteAllText("$root\marketplace\index.json", ($lines -join "`n") + "`n", [System.Text.UTF8Encoding]::new($false))
 $plugins | ForEach-Object { Write-Output ("  {0} v{1}  {2}..." -f $_.id, $_.version, $_.hash.Substring(0,16)) }
